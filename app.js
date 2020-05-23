e5130cc1786219c2a75f78f41316143ee5e68327
Pariatol <pierre.tromeur@gmail.com>
2020-05-23 23:40:14 +0200

    first

---
+++ app.js
@@ -0,0 +1,44 @@
+const menuResponsive = document.getElementById('menuResponsive');
+
+menuResponsive.addEventListener('click',()=>{
+    document.querySelector('.responsiveMenuLayout').classList.toggle('displayFlex');
+    document.querySelector('.responsiveMenuAll').classList.toggle('displayBlock');
+    document.querySelector('.closeRespMenu').classList.toggle('displayBlock');
+    document.querySelector('.openRespMenu').classList.toggle('displayNone');
+
+})
+
+document.getElementById('linkResp').addEventListener('click',()=>{
+    document.querySelector('.responsiveMenuLayout').classList.toggle('displayFlex');
+    document.querySelector('.responsiveMenuAll').classList.toggle('displayBlock');
+    document.querySelector('.closeRespMenu').classList.toggle('displayBlock')
+    document.querySelector('.openRespMenu').classList.toggle('displayNone');
+})
+
+document.getElementById('linkResp2').addEventListener('click',()=>{
+    document.querySelector('.responsiveMenuLayout').classList.toggle('displayFlex');
+    document.querySelector('.responsiveMenuAll').classList.toggle('displayBlock');
+    document.querySelector('.closeRespMenu').classList.toggle('displayBlock')
+    document.querySelector('.openRespMenu').classList.toggle('displayNone');
+})
+
+document.getElementById('linkResp3').addEventListener('click',()=>{
+    document.querySelector('.responsiveMenuLayout').classList.toggle('displayFlex');
+    document.querySelector('.responsiveMenuAll').classList.toggle('displayBlock');
+    document.querySelector('.closeRespMenu').classList.toggle('displayBlock')
+    document.querySelector('.openRespMenu').classList.toggle('displayNone');
+})
+
+document.getElementById('linkResp4').addEventListener('click',()=>{
+    document.querySelector('.responsiveMenuLayout').classList.toggle('displayFlex');
+    document.querySelector('.responsiveMenuAll').classList.toggle('displayBlock');
+    document.querySelector('.closeRespMenu').classList.toggle('displayBlock')
+    document.querySelector('.openRespMenu').classList.toggle('displayNone');
+})
+
+document.getElementById('linkResp5').addEventListener('click',()=>{
+    document.querySelector('.responsiveMenuLayout').classList.toggle('displayFlex');
+    document.querySelector('.responsiveMenuAll').classList.toggle('displayBlock');
+    document.querySelector('.closeRespMenu').classList.toggle('displayBlock')
+    document.querySelector('.openRespMenu').classList.toggle('displayNone');
+})
